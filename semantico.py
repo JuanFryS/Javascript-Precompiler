b36a4736fff50a25beb9e198bf692f1ed088a442
Juan Francisco <jfsalca@gmail.com>
2014-12-28 14:07:13 +0100

    Semantico

---
+++ semantico.py
@@ -0,0 +1,75 @@
+# Juan Francisco Salamanca Carmona #
+
+
+def scan(token):
+
+
+def main():
+
+
+def estadoP():
+
+
+def estadoPprima():
+	
+
+def estadoS():
+
+
+def estadoD():
+
+
+def estadoZ():
+
+
+def estadoI():
+
+
+def estadoSuno():
+
+
+def estadoR():
+
+
+def estadoF():
+
+
+def estadoE():
+
+
+def estadoEprima():
+
+
+def estadoT():
+
+
+def estadoTprima():
+
+
+def estadoX():
+
+
+def estadoXprima():
+
+
+def estadoG():
+
+
+def estadoGprima():	
+
+
+def estadoL():
+
+
+def estadoLprima():	
+
+
+def estadoW():
+
+
+def estadoWprima():	
+
+
+
+if __name__ == "__main__":
+	main()
