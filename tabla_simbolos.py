20520ed7ebed29fb71e2492bc06599d6351dbb97
Luis <l.ruizr04@gmail.com>
2014-12-29 02:27:32 +0100

    Primeros metodos de la tabla de simbolos, ya está la creación de la misma

--- tabla_simbolos.py
+++ tabla_simbolos.py
@@ -20,3 +20,36 @@ class Info(object):
 		self.tipo_par = tipo_par
 
 class Tabla:
+	entradas = {}
+	pal_res = ["var", "function", "return", "if", "do", "while", "true", "false", "prompt", "document.write"]
+	desplazamiento_ts_global = 0
+	desplazamiento_ts_local = 0
+	espacios = {"entero": 2, "logico": 1, "entlog": 2}
+
+	# Constructor
+	def __init__(self, general):
+		# general es un booleano que, siendo cierto, crea una tabla global
+		# Si es falso, crea una tabla local 
+
+		self.entradas = {}
+		if general:
+			for palabra in pal_res:
+				info = Info("reservada", "global", 0, 0, [])
+				self.entradas[palabra] = info
+
+	# Buscar en TS devuelve un booleano 
+	def busca_lexema(lexema):
+		dev = False
+		if lexema in entradas.keys():
+			dev = True
+		return dev
+
+	# Comprobar si un lexema es palabra reservada devuelve también un booleano
+	def es_PR(lexema):
+		dev = False
+		if lexema in pal_res:
+			dev = True
+		return dev
+
+	# Añadir identificador a la TS devuelve True si se realizo correctamente o False si no se pudo insertar
+	# Por ejemplo porque ya hay un id con ese lexema
